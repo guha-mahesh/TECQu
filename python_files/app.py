eafbfac0f26a289c70ce4db1d879163aba87ce3d
guha-mahesh <mahesh.g@northeastern.edu>
2025-02-12 18:53:30 -0500

    added Otto's html changes

---
+++ python_files/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from python_files.getData import get_streak
+
+app = Flask(__name__)
+CORS(app)  # Enables frontend to communicate with backend
+
+@app.route('/api/streak/<username>')
+def api_streak(username):
+    streak = get_streak(username)
+    return jsonify({"username": username, "streak": streak})
+
+if __name__ == '__main__':
+    app.run(debug=True)
